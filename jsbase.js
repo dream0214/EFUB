823813368685b7efa29e5df1d2d675a098db5738
dream0214 <52529597+dream0214@users.noreply.github.com>
2021-04-01 22:24:22 +0900

    Add files via upload

---
+++ jsbase.js
@@ -0,0 +1,17 @@
+
+//실습 1 객체생성
+const order={
+    orderNum : 1,
+    orderId : 'efubFront',
+    name : '이펍',
+    address : '서울특별시 서대문구 이화여대길 52',
+};
+
+for (let key in order){
+    console.log(`${key}: ${order[key]}`);
+}
+
+//실습 2 배열 내장 함수
+const onetofive=[1,2,3,4,5];
+
+const triple=onetofive.map(num=>3*num);
